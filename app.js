a8973350158826f2d930064848f42a187d7a918c
Sreeram Balamurugan <bsreeram08@gmail.com>
2020-07-10 11:46:32 +0530

    change in app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const videosRouter = require("./routers/videosRouter");
 const port = 3000;
 const host = "127.0.0.1";
 var bodyParser = require("body-parser");
-// app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/login", loginRouter);
