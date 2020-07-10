1288770664deafcffac8ecbfc24c094d6cfb278a
Sreeram Balamurugan <bsreeram08@gmail.com>
2020-07-10 13:16:02 +0530

    changes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const loginRouter = require("./Routers/loginRouter");
-const assignmentRouter = require("./routers/assignmentRouter");
-const videosRouter = require("./routers/videosRouter");
+const loginRouter = require("./routers/loginRouter.js");
+const assignmentRouter = require("./routers/assignmentRouter.js");
+const videosRouter = require("./routers/videosRouter.js");
 const port = process.env.port || 3000;
 const host = "127.0.0.1";
 var bodyParser = require("body-parser");
