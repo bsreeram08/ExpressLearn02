b002e07755f89985b16317647df673e26fa33b57
Sreeram Balamurugan <bsreeram08@gmail.com>
2020-07-10 12:07:45 +0530

    rm node modules and added secret key to git ignore

--- routers/assignmentRouter.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const assignmentPage = require("../controllers/assignmentController");
-router.post("/add", assignmentPage.addAssignment);
-router.get("/view", assignmentPage.AllAssignments);
-module.exports = router;
--- routers/videosRouter.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const videosControllers = require("../controllers/videosController");
-router.get("/getVideos", videosControllers.getVideos);
-router.post("/addVideos", videosControllers.addVideos);
-module.exports = router;
